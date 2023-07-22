63aac80da6f11fba4f30a33a39b44546bd7f23c1
Yunchi Pang <yunchipang@gmail.com>
2023-07-22 15:34:01 -0700

    Home page (#1)
    * Add links to home page self intro
    
    * Move links to constants folder
    
    * Delete projects folder

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
 
 const name = 'Yunchi Pang';
 export const siteTitle = 'yunchipang.com';
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import links from '../constants/links';
 
 export default function Home() {
   return (
@@ -11,8 +13,14 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <h3>hey, I'm yunchi 🙌🏼</h3>
         <p>👩🏻‍🎓 a software engineer based in san francisco bay area, specialized in full-stack web & mobile app development.</p>
-        <p>🌱 I am actively seeking fall 2023/summer 2024 internship/co-op opportunities in software engineering!</p>
-        <p>✨ when I am not in front of my computer screen, I train in hip-hop at @groovegenerationco, and I write blogs for triztravel.com</p>
+        <p>🌱 I am actively seeking fall 2023/summer 2024 internship/co-op opportunities in software engineering! You can contact me via {" "}
+          <Link href={links.LINKEDIN}>linkedIn</Link>.
+        </p>
+        <p>✨ when I am not in front of my computer screen, I train in hip-hop at {" "}
+          <Link href={links.GG}>@groovegenerationco</Link> 
+          {" "} and I write blogs for {" "}
+          <Link href={links.TRIZTRAVEL}>triztravel.com</Link>.
+        </p>
       </section>
     </Layout>
   );
