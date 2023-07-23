0c5d770521b44d3d5ab4f6e9f0c56a06f5365bcb
yunchipang <yunchipang@gmail.com>
2023-07-22 23:02:36 -0700

    Setup sample posts and display at home page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,19 @@ import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import links from '../constants/links';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -23,6 +34,20 @@ export default function Home() {
           {" "} and write blogs for {" "}
           <Link href={links.TRIZTRAVEL}>triztravel.com</Link>.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
