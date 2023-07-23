c40ea50ad04e7005f03513d3fad8a14ddf2e4cbe
yunchipang <yunchipang@gmail.com>
2023-07-22 23:26:31 -0700

    Polish pages and add links

--- pages/posts.js
+++ pages/posts.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 
 export async function getStaticProps() {
@@ -24,12 +26,12 @@ export default function Posts({ allPostsData }) {
 					<ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
-						{title}
+						<Link href={`/posts/${id}`}>{title}</Link>
 						<br />
-						{id}
-						<br />
-						{date}
-						</li>
+						<small className={utilStyles.lightText}>
+						  <Date dateString={date} />
+						</small>
+					  </li>
 					))}
 					</ul>
 				</section>
