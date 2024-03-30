fe44a67e62e7a478d40ef841c649eba7b0cbdc21
Yunchi Pang <yunchipang@gmail.com>
2024-03-29 18:43:41 -0700

    update about page (#57)

--- src/utils/profileLinks.js
+++ src/utils/profileLinks.js
@@ -1,7 +1,8 @@
 const LINKEDIN = 'https://www.linkedin.com/in/yunchipang/';
+const GITHUB = 'https://github.com/yunchipang';
 const GG = 'https://www.instagram.com/groovegenerationco/';
 const TRIZTRAVEL = 'https://triztravel.com/';
 
-const profileLinks = { LINKEDIN, GG, TRIZTRAVEL };
+const profileLinks = { LINKEDIN, GITHUB, GG, TRIZTRAVEL };
 
 export default profileLinks;
