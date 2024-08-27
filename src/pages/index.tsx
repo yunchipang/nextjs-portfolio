2d4c602b1ae117d96d3071c8fea7aa62b32c863a
yunchi <yunchipang@gmail.com>
2024-08-26 17:50:22 -0700

    update home page (#65)
    * update home page
    
    * delete unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import {
   faWordpress,
   faPython,
   faJs,
-  faJava,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Head from 'next/head';
@@ -25,18 +24,17 @@ export default function Home() {
         <h4>nice to meet you 🤝🏼</h4>
         <p>
           👩🏻‍🎓 a software engineer based in san francisco bay area, specialized in
-          backend development using{' '}
-          <FontAwesomeIcon icon={faPython} size="lg" />{' '}
-          <FontAwesomeIcon icon={faJs} size="lg" />{' '}
-          <FontAwesomeIcon icon={faJava} size="lg" />
+          backend development using <FontAwesomeIcon icon={faJs} size="lg" />{' '}
+          javascript / typescript and{' '}
+          <FontAwesomeIcon icon={faPython} size="lg" /> python.
         </p>
         <p>
           🤖 passionate about building software solutions that empower
           individuals & businesses to thrive.
         </p>
         <p>
-          🚀 actively seeking summer 2024 internship/co-op opportunities in
-          software engineering. feel free to contact me via{' '}
+          🚀 actively seeking 2025 new grad backend software engineer
+          opportunities. feel free to contact me via{' '}
           <Link href={profileLinks.LINKEDIN} target="_blank">
             <FontAwesomeIcon icon={faLinkedin} size="lg" />
           </Link>{' '}
@@ -44,17 +42,18 @@ export default function Home() {
           <Link href={profileLinks.GITHUB} target="_blank">
             <FontAwesomeIcon icon={faGithub} size="lg" />
           </Link>
+          .
         </p>
         <p>
-          🕺🏻 when i am not coding, i practice gymnastics, train in hip-hop at{' '}
+          🕺🏻 when i am not coding, i practice gymnastics, train in hip-hop on{' '}
           <Link href={profileLinks.GG} target="_blank">
             <FontAwesomeIcon icon={faInstagram} /> groovegeneration
           </Link>
           , and i write blogs for{' '}
           <Link href={profileLinks.TRIZTRAVEL} target="_blank">
             <FontAwesomeIcon icon={faWordpress} /> triztravel
-          </Link>
-          . drop by for a performance or an article!
+          </Link>{' '}
+          .
         </p>
         <p>💡 fun facts: i love dinosaurs 🦖🦕 and monsteras 🪴</p>
       </section>
